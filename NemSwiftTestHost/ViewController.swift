4c7c4bea48c695aad790e54b65576a7e266dfb46
Taizo Kusuda <taizok2@gmail.com>
2018-07-16 06:21:53 +0900

    refactor: separate files for test host

---
+++ NemSwiftTestHost/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  NemSwift
+//
+//  Created by Kazuya Okada on 2017/11/15.
+//  Copyright © 2017年 OpenApostille. All rights reserved.
+//
+
+import UIKit
+import APIKit
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
